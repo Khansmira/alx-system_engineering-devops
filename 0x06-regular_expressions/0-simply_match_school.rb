4683364b3aaac9585a3efeb67d09a0dbfbc1b0de
Khansmira <miriamakanwa@gmail.com>
2023-09-05 12:51:40 -0700

    regex school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,6 +1,3 @@
 #!/usr/bin/env ruby
 #This script checks for the word 'school'
-input_text = ARGV[0]
-regex = /school/
-  match = input_text.scan(regex)
-puts match.join
+puts ARGV[0].scan(/School/).join
