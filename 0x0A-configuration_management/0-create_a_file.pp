617fe79058a383588ad7d54d792e0383184874bd
Khansmira <miriamakanwa@gmail.com>
2023-10-27 10:51:15 -0700

    Puppet manifest: create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Manifest that creates a file in /tmp folder
 file { '/tmp/school':
     ensure  => 'file',
+    content => 'I Love Puppet',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    content => 'I Love Puppet',
 }
